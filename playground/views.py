323a247a84b916b6c4d95d1d11ebd55f1578df69
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-19 17:33:28 -0700

    using dango debug toolbar in application on run, Organizing models for ecommerce app and defining classes in models, connecting to mysql and generating dummy data for tables

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
-
+from django.db.models import Q, F, Func, Value
+from django.db.models.functions import Concat
+from store.models import Product, OrderItem, Customer, Collection
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    collection = Collection(pk=11)
+    collection.title = 'Games'
+    collection.featured_product = None
+    collection.save()
+
+
+
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'products' : list(queryset)})
