dbed1f166fdb20e16eb0d64630ba14ecbdecf446
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-20 17:16:48 -0700

    created api views and serializers utilizing rest_framework

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from.serializers import ProductSerializer
+from django.db import models
 
 # Create your views here.
+
+@api_view()
+def product_list(request):
+    queryset =Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
+
+@api_view()
+def product_detail(response, id):
+        product = get_object_or_404(Product, id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+
