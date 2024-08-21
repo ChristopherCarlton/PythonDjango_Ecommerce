501f8352dc7092276ee72521152a50655ac6a75c
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-20 21:55:26 -0700

    cleaned up imports and useless code

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-# from rest_framework.routers import SimpleRouter, DefaultRouter
+from django.urls.conf import include
+from rest_framework_nested import routers
 from . import views
-from rest_framework_nested import routers, NestedDefaultRouter
 
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
@@ -10,4 +10,5 @@ router.register('collections', views.CollectionViewSet)
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
+# URLConf
 urlpatterns = router.urls + products_router.urls
--- store/views.py
+++ store/views.py
@@ -1,14 +1,14 @@
+from store.pagination import DefaultPagination
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status
-from .models import Collection, Product, OrderItem, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
-from rest_framework.viewsets import ModelViewSet
-from .filters import ProductFilter
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import DefaultPagination
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
+from .filters import ProductFilter
+from .models import Collection, Product, Review
+from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -22,29 +22,33 @@ class ProductViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'request': self.request}
-    
-    def destroy(self, request, *args, **kwargs):
-        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        return super.destroy({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class CollectionViewSet(ModelViewSet):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
-        collection = get_object_or_404(Product, pk=pk)
-        if collection.orderitems.count() > 0:
-            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection = get_object_or_404(Collection, pk=pk)
+        if collection.products.count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
+
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
 
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
-    
 
     def get_serializer_context(self):
-        return{'product_id': self.kwargs['product_pk']}
+        return {'product_id': self.kwargs['product_pk']}
