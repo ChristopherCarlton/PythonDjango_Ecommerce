b8c5b63093ec63afa1d02c2cca009b5a89e37450
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-22 17:15:15 -0700

    created the order viewset as well order serializer for creating orders and associations with shopping carts and order objects.

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
-from store.models import Cart, CartItem, Product, Collection, Review, Customer
+from django.db import transaction
+from .models import Cart, CartItem, Customer, Product, Collection, Review, Order, OrderItem
 from rest_framework import serializers
 
 
@@ -100,6 +101,44 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(read_only=True)
+
     class Meta:
-        model = Customer 
-        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
+        model = Customer
+        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']   
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ['id', 'product', 'unit_price', 'quantity']  
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    class Meta:
+        model = Order 
+        fields = ['id', 'customer', 'placed_at', 'payment_status', 'items']
+
+class CreateOrderSerializer(serializers.Serializer):
+    cart_id = serializers.UUIDField()
+
+    def save(self, **kwargs):
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            (customer, created)  = Customer.objects.get_or_create(user_id=self.context['user_id'])
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects.select_related('product').filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price = item.product.unit_price,
+                    quantity = item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            Cart.objects.filter(pk=cart_id).delete()
+
+            return order
+    
--- storefront/settings.py
+++ storefront/settings.py
@@ -147,17 +147,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
-
 
 AUTH_USER_MODEL = 'core.User'
 
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
-        'current_user': 'core.serializers.UserSerializer'
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
 
