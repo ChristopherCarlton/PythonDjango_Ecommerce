e850a706862a956d12492f7576015680c7d449d8
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-23 18:27:39 -0700

    installed celery to run tasks in background. Implemented in playground views

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,20 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+# from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+# from templated_mail.mail import BaseEmailMessage
 from django.shortcuts import render
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
+# sending an email
+# def say_hello(request):
+#     try:
+#         message = BaseEmailMessage(
+#             template_name='emails/hello.html',
+#             context={'name': 'Mosh'}
+#         )
+#         message.send(['john@moshbuy.com'])
+#     except BadHeaderError:
+#         pass 
+#     return render(request, 'hello.html', {'name': 'Mosh'})
 
 def say_hello(request):
-    try:
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={'name': 'Mosh'}
-        )
-        message.send(['john@moshbuy.com'])
-    except BadHeaderError:
-        pass 
+    notify_customers.delay('Hello')
     return render(request, 'hello.html', {'name': 'Mosh'})
