7665d855300d7e1ea736d5a3a912fce37c6f39d0
ChristopherCarlton <ccarlton2001@gmail.com>
2024-08-25 13:00:09 -0700

    added caching on data to ensure faster response times

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,12 @@
 # from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 # from templated_mail.mail import BaseEmailMessage
 from django.shortcuts import render
-from .tasks import notify_customers
-
+# from .tasks import notify_customers
+import requests
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 # sending an email
 # def say_hello(request):
 #     try:
@@ -15,6 +19,9 @@ from .tasks import notify_customers
 #         pass 
 #     return render(request, 'hello.html', {'name': 'Mosh'})
 
-def say_hello(request):
-    notify_customers.delay('Hello')
-    return render(request, 'hello.html', {'name': 'Mosh'})
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def get(self,request):
+        response = requests.get('https://httpbin.org/delay/2')
+        data = response.json()
+        return render(request, 'hello.html', {'name': data})
